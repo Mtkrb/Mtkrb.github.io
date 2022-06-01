235c4883d81f7c00fb24f1f58f28641d81ee8505
Bektemir Abilov <younghearth@gmail.com>
2022-06-02 05:17:47 +0600

    some changes

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function  Header() {
+    return (
+        <header><div className="settings" href="/"></div></header>
+    )
+}
+
+export default Header
---
+++ src/components/Timer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Buttons from "./Buttons";
+
+function Timer() {
+  return (
+    <div id="container">
+      <div id="content">
+        <div id="checks">
+          <span className="check">
+            <i classNamey="fa fa-check fa-2x" aria-hidden="true"></i>
+          </span>
+          <span className="check">
+            <i className="fa fa-check fa-2x" aria-hidden="true"></i>
+          </span>
+          <span className="check">
+            <i className="fa fa-check fa-2x" aria-hidden="true"></i>
+          </span>
+          <span className="check">
+            <i className="fa fa-check fa-2x" aria-hidden="true"></i>
+          </span>
+        </div>
+
+        <div>
+          <div id="timer-container">
+            <span id="timer" className="large">
+              25:00
+            </span>
+          </div>
+          <Buttons />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Timer
